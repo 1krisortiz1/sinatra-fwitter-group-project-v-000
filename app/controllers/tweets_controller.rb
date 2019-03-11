4b0482b301e8209580885049341971b950628a44
Kristin Ortiz <itskristin.ortiz@gmail.com>
2019-03-11 17:42:23 +0000

    Automatically backed up by Learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -41,7 +41,6 @@ class TweetsController < ApplicationController
 #update
 
     get 'tweets/:id/edit' do
-        binding.pry
         if logged_in?
             @tweet = Tweet.find(params[:id])
             if @tweet.user.username == current_user.username
